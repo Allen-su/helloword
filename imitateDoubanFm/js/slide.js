7d8e27a18daaf592614001ce81111796a873267d
Allen <yunan.js@gmail.com>
2014-12-30 12:32:20 +0800

    add touch move

--- imitateDoubanFm/js/slide.js
+++ imitateDoubanFm/js/slide.js
@@ -11,11 +11,11 @@ define(function(require, exports, module){
 
 	var dragDrop = function() {
 		var draging = null,
-			initY = 0;
+			initY = 0,
+			curHeight = 0;
 		function handler( e ) {
 			e = e || window.event;
-			var target = e.target || e.srcElement,
-				curHeight = 0;
+			var target = e.target || e.srcElement;
 			switch( e.type ){
 				case 'mousedown' :
 					if ( target == controlBox[0] || target == footer[0] ) {
@@ -63,5 +63,67 @@ define(function(require, exports, module){
 			}
 		};
 	};
+
+	var touchMove = function() {
+		var draging = null,
+			initY = 0,
+			curHeight = 0;
+		function handler( e ){
+			e = e || window.event;
+			var target = e.target || e.srcElement;
+				
+			switch ( e.type ){
+				case 'touchstart' :
+					if ( target == controlBox[0] || target == footer[0] ) {
+						draging = controlBox;
+					}
+					initY = e.touches[0].pageY;
+					break;
+				case 'touchmove' :
+					curHeight = controlBox.height();
+					var distance = initY -  e.changedTouches[0].pageY;
+					if ( draging !== null && 
+							( ( curHeight < slideParam.maxHeight && curHeight > slideParam.minHeight ) || 
+								( curHeight == slideParam.maxHeight && distance < 0 ) || 
+								( curHeight == slideParam.minHeight && distance > 0 ) )) {
+						controlBox.height( controlBox.height() + initY -  e.changedTouches[0].pageY );
+						initY = e.changedTouches[0].pageY;
+					}
+					e.preventDefault();
+					break;
+
+				case 'touchend' :
+					curHeight = controlBox.height();
+					if ( curHeight >= slideParam.autoDistance ) {
+						controlBox.animate({height:slideParam.maxHeight});
+					} else {
+						controlBox.animate({height:slideParam.minHeight});
+					}
+					draging = null;
+					initY = 0;
+					break;
+			}
+		}
+		return { 
+			enable : function(){
+				document.addEventListener('touchstart', handler, false);
+				document.addEventListener('touchmove', handler, false);
+				document.addEventListener('touchend', handler, false);
+			},
+			disable : function(){
+				document.removeEventListener('touchstart', handler);
+				document.removeEventListener('touchmove', handler);
+				document.removeEventListener('touchend', handler);
+			}
+		};
+
+	}
+
+
+
+
+
+
 	dragDrop().enable();
+	touchMove().enable();
 });
