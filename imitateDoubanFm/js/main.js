bea36b7359e2ff3880c5ecab82623bb70f11dae1
Allen <yunan.js@gmail.com>
2015-01-07 00:00:04 +0800

    height auto

--- imitateDoubanFm/js/main.js
+++ imitateDoubanFm/js/main.js
@@ -1,5 +1,13 @@
 define(function(require, exports, module){
 	require('navigation');
 	require('slide');
+
+	$(function(){
+		var body = window.screen.availHeight,
+			header = $('header').height(),
+			nav = $('#find_program > nav').height(),
+			footer = $('#control_box').height();
+		$('#channel dl').height( body - header - nav - footer );
+	});
 	
 });
