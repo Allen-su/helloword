d74f84377b279eeab43580a4061bc8a3d1dfccfb
Allen <yunan.js@gmail.com>
2015-01-08 23:41:23 +0800

    fix the height issue on pc

--- imitateDoubanFm/js/main.js
+++ imitateDoubanFm/js/main.js
@@ -3,11 +3,11 @@ define(function(require, exports, module){
 	require('slide');
 
 	$(function(){
-		var body = window.screen.availHeight,
+		var body = document.body.clientHeight,
 			header = $('header').height(),
 			nav = $('#find_program > nav').height(),
 			footer = $('#control_box').height();
-		$('#channel dl').height( body - header - nav - footer - 10);
+		$('#channel dl').height( body - header - nav - footer);
 	});
 	
 });
