710c0228fdc13c770520bf3250f0333ab95b2042
Allen <yunan.js@gmail.com>
2015-01-11 19:57:16 +0800

    add resize event

--- imitateDoubanFm/js/main.js
+++ imitateDoubanFm/js/main.js
@@ -3,11 +3,19 @@ define(function(require, exports, module){
 	require('slide');
 
 	$(function(){
-		var body = document.body.clientHeight,
-			header = $('header').outerHeight(true),
-			nav = $('#find_program > nav').outerHeight(true),
-			footer = $('#control_box').outerHeight(true);
-		$('#channel dl').height( body - header - nav - footer);
+		var body = document.body,
+			header = $('header'),
+			nav = $('#find_program > nav'),
+			footer = $('#control_box'),
+			dl = $('#channel dl');
+		dl.height( body.clientHeight - header.outerHeight(true) - 
+			nav.outerHeight(true) - footer.outerHeight(true));
+		window.onresize = function(){
+			clearTimeout( arguments.callee.timeId );
+			arguments.callee.timeId = setTimeout(function(){
+				dl.height( body.clientHeight - header.outerHeight(true) - 
+					nav.outerHeight(true) - footer.outerHeight(true));
+			},100);
+		};
 	});
-	
 });
